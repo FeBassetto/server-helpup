5b0ec0b32d93141fdd627619c2cb9debea6a654e
felipe bassetto <felipebtu9@gmail.com>
2023-10-25 15:57:42 -0300

    main - structuring project

---
+++ src/http/controllers/users/routes.ts
@@ -0,0 +1,7 @@
+import { FastifyInstance } from 'fastify'
+
+export async function usersRoutes(app: FastifyInstance) {
+  app.get('/hello-world', (request, reply) => {
+    return reply.status(201).send('Hello World')
+  })
+}
