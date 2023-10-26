11a19ae77695221ff5fcae36337fe422795dcfb4
felipe bassetto <felipebtu9@gmail.com>
2023-10-25 22:53:04 -0300

    main - testing github actions

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,16 @@
 # Atualize o código do GitHub
 git pull
 
-# Execute o build do aplicativo (substitua 'yarn build' pelo comando apropriado)
+# Execute o build do aplicativo
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+source ~/.profile
+nvm ls-remote
+nvm install 18.16.1
+nvm use 18.16.1
+
+npm i yarn pm2 -g
+
 yarn build
 
-# Reinicie o PM2 (substitua 'pm2 restart' pelo comando apropriado)
+# Reinicie o PM2
 pm2 restart server
