b868552f8cb6546f6398252acafa10fb5d537df2
felipe bassetto <felipebtu9@gmail.com>
2023-10-25 22:43:20 -0300

    main - testing github actions

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+# Atualize o código do GitHub
+git pull
+
+# Execute o build do aplicativo (substitua 'yarn build' pelo comando apropriado)
+yarn build
+
+# Reinicie o PM2 (substitua 'pm2 restart' pelo comando apropriado)
+pm2 restart server
