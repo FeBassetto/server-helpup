4d3a02e424e4a130c986c0de458ae28488454466
felipe bassetto <felipebtu9@gmail.com>
2023-10-25 23:32:00 -0300

    main - testing github actions

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -2,6 +2,6 @@ import { FastifyInstance } from 'fastify'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.get('/hello-world', (request, reply) => {
-    return reply.status(201).send('Hello World3')
+    return reply.status(201).send('Hello World!!!!!')
   })
 }
