f4bd8d1a2e4a3bb010ea97473cdfe91daf8bf7ef
felipe bassetto <felipebtu9@gmail.com>
2023-10-31 00:01:02 -0300

    main - test nginx

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -2,6 +2,6 @@ import { FastifyInstance } from 'fastify'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.get('/hello-world', (request, reply) => {
-    return reply.status(201).send('Hello World!!!!!')
+    return reply.status(201).send('Hello World!')
   })
 }
