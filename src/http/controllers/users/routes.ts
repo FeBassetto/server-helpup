96d6d29567a292c9b1128dd7bcf0da043792982d
felipe bassetto <felipebtu9@gmail.com>
2023-10-31 08:27:00 -0300

    main - starting to use docker

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,7 +1,12 @@
+import { prisma } from '@/lib/prisma'
 import { FastifyInstance } from 'fastify'
 
 export async function usersRoutes(app: FastifyInstance) {
-  app.get('/hello-world', (request, reply) => {
+  app.get('/hello-world', async (request, reply) => {
+    const teste = await prisma.user.findMany({})
+
+    console.log(teste)
+
     return reply.status(201).send('Hello World!')
   })
 }
