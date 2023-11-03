4043c21c37ef65f5cbcff8168d8565b6ccee10a9
felipe bassetto <felipebtu9@gmail.com>
2023-11-03 14:51:53 -0300

    main - config aws

--- src/http/controllers/ping/routes.ts
+++ src/http/controllers/ping/routes.ts
@@ -2,22 +2,22 @@ import { FastifyInstance } from 'fastify'
 
 export async function pingRoutes(app: FastifyInstance) {
   app.get('/ping', async (request, reply) => {
-    return reply.status(201).send({ status: 'ok', host: 'AWS' })
+    return reply.status(201).send({ status: 'ok', host: 'AWS-SP' })
   })
 
   app.post('/ping', async (request, reply) => {
-    return reply.status(201).send({ status: 'ok', host: 'AWS' })
+    return reply.status(201).send({ status: 'ok', host: 'AWS-SP' })
   })
 
   app.put('/ping', async (request, reply) => {
-    return reply.status(201).send({ status: 'ok', host: 'AWS' })
+    return reply.status(201).send({ status: 'ok', host: 'AWS-SP' })
   })
 
   app.delete('/ping', async (request, reply) => {
-    return reply.status(201).send({ status: 'ok', host: 'AWS' })
+    return reply.status(201).send({ status: 'ok', host: 'AWS-SP' })
   })
 
   app.patch('/ping', async (request, reply) => {
-    return reply.status(201).send({ status: 'ok', host: 'AWS' })
+    return reply.status(201).send({ status: 'ok', host: 'AWS-SP' })
   })
 }
