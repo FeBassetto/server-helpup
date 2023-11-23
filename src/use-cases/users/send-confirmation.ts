ca2ce39c985903e6ec411e2619c4982c176846d2
felipe bassetto <felipebtu9@gmail.com>
2023-11-23 17:02:27 -0300

    Adjusting build into deploy

--- src/use-cases/users/send-confirmation.ts
+++ src/use-cases/users/send-confirmation.ts
@@ -1,8 +1,7 @@
-import { resolve } from 'path'
-
 import { env } from '@/env'
 
 import { MailProvider } from '@/shared/providers/MailProvider/mail-provider'
+import { generateConfirmationEmailHtml } from '@/views/emails/confirm-email'
 
 interface SendConfirmationUseCaseRequest {
   email: string
@@ -13,26 +12,14 @@ export class SendConfirmationUseCase {
   constructor(private mailProvider: MailProvider) {}
 
   async execute({ email, token }: SendConfirmationUseCaseRequest) {
-    const templatePath = resolve(
-      __dirname,
-      '..',
-      '..',
-      'views',
-      'emails',
-      'confirm-email.hbs',
-    )
-
-    const variables = {
+    const props = {
       link: `${env.BASE_URL}/${env.CONFIRM_MAIL_URL}/${token}`,
       siteurl: `${env.BASE_URL}`,
       logo: `${env.LOGO_URL}`,
     }
 
-    await this.mailProvider.sendMail(
-      email,
-      'Confirmação de email',
-      variables,
-      templatePath,
-    )
+    const html = generateConfirmationEmailHtml(props)
+
+    await this.mailProvider.sendMail(email, 'Confirmação de email', html)
   }
 }
