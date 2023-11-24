e6c943e8e1dc63880021852aec129878aed30031
felipe bassetto <felipebtu9@gmail.com>
2023-11-23 23:05:01 -0300

    adjusting ping

--- src/http/controllers/ping/routes.ts
+++ src/http/controllers/ping/routes.ts
@@ -2,22 +2,32 @@ import { FastifyInstance } from 'fastify'
 
 export async function pingRoutes(app: FastifyInstance) {
   app.get('/ping', async (request, reply) => {
-    return reply.status(201).send({ status: 'ok', host: 'AWS-SP' })
+    return reply
+      .status(201)
+      .send({ status: 'ok', host: 'AWS-SP', version: '1.0.0' })
   })
 
   app.post('/ping', async (request, reply) => {
-    return reply.status(201).send({ status: 'ok', host: 'AWS-SP' })
+    return reply
+      .status(201)
+      .send({ status: 'ok', host: 'AWS-SP', version: '1.0.0' })
   })
 
   app.put('/ping', async (request, reply) => {
-    return reply.status(201).send({ status: 'ok', host: 'AWS-SP' })
+    return reply
+      .status(201)
+      .send({ status: 'ok', host: 'AWS-SP', version: '1.0.0' })
   })
 
   app.delete('/ping', async (request, reply) => {
-    return reply.status(201).send({ status: 'ok', host: 'AWS-SP' })
+    return reply
+      .status(201)
+      .send({ status: 'ok', host: 'AWS-SP', version: '1.0.0' })
   })
 
   app.patch('/ping', async (request, reply) => {
-    return reply.status(201).send({ status: 'ok', host: 'AWS-SP' })
+    return reply
+      .status(201)
+      .send({ status: 'ok', host: 'AWS-SP', version: '1.0.0' })
   })
 }
